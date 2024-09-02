410c69609251c8f9e6a39af3bacf3ce88c630e9f
Snochi35 <levanianoize@gmail.com>
2024-09-02 19:39:44 +0300

    Add files via upload

---
+++ Task_Strings_Indexing_fixed.py
@@ -0,0 +1,14 @@
+example = "Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:]) #исправить
+l = len(example) + 1
+fix_1 = example[l//2:]
+print(fix_1)
+print(example[::-1])
+print(example[1:10:2]) #исправить
+print(example[1::2]) #1
+
+
+
+
